1a5d15d8346cd57868b6e23635113d8f73640b40
Will Robinson <will@oznetnerd.com>
2020-03-15 22:15:34 +1100

    Enabled the use of **kwargs

--- onnpdf/pdf.py
+++ onnpdf/pdf.py
@@ -1,24 +1,17 @@
 import os
 from weasyprint import HTML
-from jinja2 import Template
+import jinja2
 
 
-def create_pdf(template_file_path, template_assets_dir, output_file_path, kv_pairs):
-    """Converts Jinja2 templates to PDF
-
-    :param template_file_path: Path to template filename
-    :param template_assets_dir: Assets used by the template
-    :param output_file_path: Output file path
-    :param kv_pairs: Jinja2 key/value pairs
-
-    :return: None
-    """
+def create_pdf(template_dir, template_filename, template_assets_dir, output_file_path, **kv_pairs):
+    """Converts Jinja2 templates to PDF"""
 
     output_dir = os.path.dirname(output_file_path)
     os.makedirs(output_dir, exist_ok=True)
 
-    with open(template_file_path, 'r') as file:
-        template_html = Template(file.read())
-        rendered_html = template_html.render(kv_pairs)
+    template_loader = jinja2.FileSystemLoader(searchpath=template_dir)
+    template_env = jinja2.Environment(loader=template_loader)
+    template_html = template_env.get_template(template_filename)
+    rendered_html = template_html.render(**kv_pairs)
 
-        HTML(string=rendered_html, base_url=template_assets_dir).write_pdf(output_file_path)
+    HTML(string=rendered_html, base_url=template_assets_dir).write_pdf(output_file_path)
